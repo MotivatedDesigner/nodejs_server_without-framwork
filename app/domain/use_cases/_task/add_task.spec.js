2a10926eee54e317be45998d15a73e73afa19d0c
Ayoub Elmendoub <76922728+MotivatedDesigner@users.noreply.github.com>
2022-01-01 20:57:05 +0100

    test: findById

--- app/domain/use_cases/_task/add_task.spec.js
+++ app/domain/use_cases/_task/add_task.spec.js
@@ -1,14 +1,18 @@
 import makeAddTask from './add_task.js'
-
 import {fakeTask, fakeTaskDatabase} from '../../../../__test__/index.js'
+import buildTaskEntity from "../../entities/_task/task.js"
+import { Id, Sanitizer } from "../../../../utils/index.js"
+
+const taskEntity = buildTaskEntity(Id, Sanitizer)
 
 describe('add Task', () => {
 
-  it('inserts task in the database', async () => {
-    const fakeTask = fakeTask()
+  it('inserts task with existing id', async () => {
+    const id = 'testId'
+    const testTask = fakeTask({id})
     const addTask = makeAddTask(taskEntity, fakeTaskDatabase)
-    const inserted = await addTask(fakeTask)
-    // expect(inserted).toMatchObject(fakeTask)
+    const inserted = await addTask(testTask)
+    expect(inserted.id).toEqual(id)
   })
 
 })
