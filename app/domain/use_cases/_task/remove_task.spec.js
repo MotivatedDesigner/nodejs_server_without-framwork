ceaa07d1816eddc27d45fb35df8e9cf673e5bddd
Ayoub Elmendoub <76922728+MotivatedDesigner@users.noreply.github.com>
2022-01-02 13:25:51 +0100

    test_done: use_case remove_task

--- app/domain/use_cases/_task/remove_task.spec.js
+++ app/domain/use_cases/_task/remove_task.spec.js
@@ -9,4 +9,20 @@ describe('remove Task', () => {
     expect(removeTask()).rejects.toThrow('You must supply a task id')
   })
 
+  it('must delete nothing if task does not exist', async () => {
+    const deleteMessage = await removeTask("doesNotExist")
+    expect(deleteMessage).toMatchObject({
+      "deletedCount": 0, 
+      "message": "Task not found, nothing to delete"
+    })
+  })
+
+  it('must delete task if exist', async () => {
+    const deleteMessage = await removeTask("doesExist")
+    expect(deleteMessage).toMatchObject({
+      "deletedCount": 1,
+      "message": "Task deleted successfully"
+    })
+  })
+
 })
