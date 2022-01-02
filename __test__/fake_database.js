8cf8bbcc37677ebd1d77ffc4173603c0b402ad5c
Ayoub Elmendoub <76922728+MotivatedDesigner@users.noreply.github.com>
2022-01-02 13:37:54 +0100

    adapter: fake_databese add: findByProjectId()

--- __test__/fake_database.js
+++ __test__/fake_database.js
@@ -7,6 +7,11 @@ const saveDb = () => fs.writeFileSync('./__test__/db.json', JSON.stringify(db))
 
 const findByIdGeneric = table => id => Promise.resolve( db[table].find( el => el.id === id ) )
 
+const findByProjectId = projectId => {
+  const tasks = db["tasks"].filter( task => task.projectId === projectId )
+  Promise.resolve(tasks)
+}
+
 const insert = object => Promise.resolve(object)
 
 const update = object => Promise.resolve(object)
@@ -16,6 +21,7 @@ const remove = id => Promise.resolve(true)
 export default Object.freeze({
   tasks: Object.freeze({
     findById: findByIdGeneric("tasks"),
+    findByProjectId,
     insert,
     update,
     remove
